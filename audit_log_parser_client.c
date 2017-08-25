2c7eef9f272e7a9d97569a717f8cf76cdef409fc
Roman Storozhenko <romeusmeister@gmail.com>
2017-08-25 16:16:12 +0300

    Adds events bufferization to avoid big http overhead

--- audit_log_parser_client.c
+++ audit_log_parser_client.c
@@ -10,46 +10,41 @@
 #include <sys/stat.h>
 #include <json/json.h>
 #include <curl/curl.h>
+#include <stdbool.h>
 
-#define BUFFLEN 524288
+#define BUF_LEN 524288
+#define RECORD_BUF_LEN 102400
 #define MAX_DESCRIPTORS 8192
 #define CONNECTION_URL "localhost:8888"
+#define POLLING_DELAY 1
 
-static char temporary_buffer[BUFFLEN];
-
-char *fetch_next_event(auparse_state_t *au, char *buf)
+void fetch_next_event(auparse_state_t *au, json_object *json)
 {
-	const char *record;
-
-	json_object *jobj = json_object_new_object();
-	*buf = '\0';
-
 	do {
 		const char *type = auparse_get_type_name(au);
 		json_object *json_type = json_object_new_string(type);
 
-		json_object_object_add(jobj, "type", json_type);
+		json_object_object_add(json, "type", json_type);
 
 		do {
 			json_object *json_field_value = json_object_new_string(auparse_get_field_str(au));
-			json_object_object_add(jobj, auparse_get_field_name(au), json_field_value);
+			json_object_object_add(json, auparse_get_field_name(au), json_field_value);
 		} while (auparse_next_field(au) > 0);
 
-		strcat(buf, json_object_to_json_string(jobj));
-		strcat(buf, ",\n\n");
-
 	} while (auparse_next_record(au) > 0);
-
-	return buf;
 }
 
 int main(int argc, char *argv[])
 {
 	int err;
+	int free_buf_space;
 	auparse_state_t *au;
 	CURL *curl;
 	CURLcode res;
 	struct curl_slist *headers = NULL;
+	static char temp_buf[BUF_LEN], record_buf[RECORD_BUF_LEN];
+	bool buf_overflow;
+
 
 	curl_global_init(CURL_GLOBAL_ALL);
 
@@ -77,19 +72,48 @@ int main(int argc, char *argv[])
 			exit(1);
 		}
 
-		do {
-			fetch_next_event(au, temporary_buffer);
+		temp_buf[0] = '\0';
+		buf_overflow = false;
 
-			curl_easy_setopt(curl, CURLOPT_POSTFIELDS, temporary_buffer);
+		for (;;) {
+			while (auparse_next_event(au) > 0 && !buf_overflow) {
+				json_object *json = json_object_new_object();
 
-			res = curl_easy_perform(curl);
+				fetch_next_event(au, json);
 
-			if (res != CURLE_OK)
-				syslog(LOG_ERR, "Transfer to the server has failed: %s", curl_easy_strerror(res));
+				record_buf[0] = '\0';
+				strcat(record_buf, json_object_to_json_string(json));
+				free_buf_space  = BUF_LEN - strlen(temp_buf) - 2;
 
-		} while (auparse_next_event(au) > 0);
+				if (free_buf_space >= strlen(record_buf)) {
+					strcat(temp_buf, record_buf);
+					strcat(temp_buf, ",\n\n");
+				} else {
+					buf_overflow = true;
+				}
 
-		auparse_destroy(au);
+				json_object_put(json);
+			};
+
+			if (strlen(temp_buf)) {
+				curl_easy_setopt(curl, CURLOPT_POSTFIELDS, temp_buf);
+				res = curl_easy_perform(curl);
+
+				if (res != CURLE_OK)
+					syslog(LOG_ERR, "Transfer to the server has failed: %s", curl_easy_strerror(res));
+			}
+
+			if (buf_overflow) {
+				strcat(temp_buf, record_buf);
+				strcat(temp_buf, ",\n\n");
+				buf_overflow = false;
+			} else {
+				sleep(POLLING_DELAY);
+			}
+
+			temp_buf[0] = '\0';
+		}
+
 	} else {
 		syslog(LOG_ERR, "curl library initialization has failed");
 	}
